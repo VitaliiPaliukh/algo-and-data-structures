a193f1b501fa63903ea08b6ebcb95dc52c0c79ca
VitaliiPaliukh <vitalikpaliukh@gmail.com>
2024-05-03 10:20:57 +0300

    add typization and rename the name of variable

--- src/routing_internet.py
+++ src/routing_internet.py
@@ -1,17 +1,23 @@
 import csv
+from typing import List, Tuple, Set
 
 
-def read_data(input_file_direction, output_file_direction):
-    edges = []
-    vertices = set()
+def read_data(input_file_direction: str, output_file_direction: str) -> Tuple[List[Tuple[str, str, int]], Set[str]]:
+    """
+    :param input_file_direction: the direction of the input file
+    :param output_file_direction: the direction of the output file
+    :return: a list of all vertices and a list of edges in the format (k1, k2, weight)
+     """
+    edges: List[Tuple[str, str, int]] = []
+    vertices: Set[str] = set()
     with open(input_file_direction, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             if len(row) == 3:
-                k1, k2, distance = row[0], row[1], int(row[2])
-                edges.append((k1, k2, distance))
-                vertices.add(k1)
-                vertices.add(k2)
+                cur_vertex, next_vertex, distance = row[0], row[1], int(row[2])
+                edges.append((cur_vertex, next_vertex, distance))
+                vertices.add(cur_vertex)
+                vertices.add(next_vertex)
     dist_to = prim_algorithm(edges, vertices)
     write_output(output_file_direction, dist_to)
     return edges, vertices
@@ -22,15 +28,20 @@ def write_output(output_file_direction, dist_to):
         file_write.write(str(dist_to))
 
 
-def prim_algorithm(edges, vertices):
+def prim_algorithm(edges: List[Tuple[str, str, int]], vertices: Set[str]) -> int:
+    """
+    :param edges:
+    :param vertices:
+    :return: -1 if set(vertices) != visited: else return the sum of the weights
+    """
     weights = {vertex: float('inf') for vertex in vertices}
     start_vertex = min(vertices)
     weights[start_vertex] = 0
     queue = [start_vertex]
     visited = {start_vertex}
-
+#todo: use priority queue
     while queue:
-        min_vertex = min(queue, key=weights.get)
+        min_vertex = min(queue, key=weights.get)  # rename min_vertex to min_edge
         queue.remove(min_vertex)
         for edge in edges:
             if edge[0] == min_vertex and edge[1] not in visited and weights[edge[1]] > edge[2]:
@@ -46,6 +57,3 @@ def prim_algorithm(edges, vertices):
         return -1
 
     return sum(weights.values())
-
-
-read_data("../resource/input_undirected.csv", "../resource/output_undirected_route.txt")
--- test/test_routing_internet.py
+++ test/test_routing_internet.py
@@ -3,14 +3,14 @@ import unittest
 
 
 class TestRoutingInternet(unittest.TestCase):
-    def test_file(self):
+    def test_normal_case(self):
         read_data("../resource/input_route.csv", "../resource/output_route.txt")
         file = open("../resource/output_route.txt")
         result = int(file.readline())
         file.close()
         self.assertEqual(result, 900)
 
-    def test_undirect_file(self):
+    def test_undirected_graph(self):
         read_data("../resource/input_undirected.csv", "../resource/output_undirected_route.txt")
         file = open("../resource/output_undirected_route.txt")
         result = int(file.readline())
